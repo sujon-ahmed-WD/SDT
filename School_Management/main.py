dafaf2707f7edcdee68cd0da9d0839bbedb9f02c
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-04-22 22:13:28 +0600

    ok

--- School_Management/main.py
+++ School_Management/main.py
@@ -3,15 +3,52 @@ from person import Student, Teacher
 from Subject import Subject
 from classroom import ClassRoom
 
+school = School("ABC", "Dhaka")
 
-school=School("Abs","Dhaka")
+# Adding Classroom
+eight = ClassRoom("Eight")
+nine = ClassRoom("Nine")
+ten = ClassRoom("Ten")
 
-# Additing Classroom
+school.add_classroom(eight)
+school.add_classroom(nine)
+school.add_classroom(ten)
 
-eight=ClassRoom("Eight") 
-Nine=ClassRoom("nine") 
-ten=ClassRoom("Ten")
+# Adding Student
+rahim = Student("Rahim", eight)
+karim = Student("Karim", nine)
+fahim = Student("Fahim", ten)
+hamim = Student("Hamim", ten)
 
-school.add_classroom(eight) 
-school.add_classroom(Nine) 
-school.add_classroom(ten) 
+school.student_admission(rahim)
+school.student_admission(karim)
+school.student_admission(fahim)
+school.student_admission(hamim)
+
+# Adding Teachers
+abul = Teacher("Abul Khan")
+babul = Teacher("Babul Khan")
+kabul = Teacher("Kabul Khan")
+
+# Adding Subjects
+bangla = Subject("Bangla", abul)
+physics = Subject("Physics", babul)
+chemistry = Subject("Chemistry", kabul)
+biology = Subject("Biology", kabul)
+
+eight.add_subject(bangla)
+eight.add_subject(physics)
+eight.add_subject(chemistry)
+nine.add_subject(biology)
+nine.add_subject(physics)
+nine.add_subject(chemistry)
+ten.add_subject(chemistry)
+ten.add_subject(physics)
+ten.add_subject(bangla)
+ten.add_subject(biology)
+
+eight.take_samister_final()
+nine.take_samister_final()
+ten.take_samister_final()
+
+print(school)
