a901417111330eaacc61767546fbec729a1e6693
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-04-23 19:36:21 +0600

    owern

--- week3/Con1.py/online_food_order.py
+++ week3/Con1.py/online_food_order.py
@@ -15,12 +15,20 @@ class Food_Item(ABC):
     def prepare(self):
         pass
     
+
+class Restrurant_Owener:
+    orderlist=[]
     
+    def add_orders(self,order):
+        self.orderlist.append(order)
 class Burger(Food_Item):
     def __init__(self):
         super().__init__("Burger", 150)
     def prepare(self):
         print( "Grilling the burger patty and adding sauce ... ")
+        
+    def __repr__(self):
+        return self.get_name()
 
 class Pizza(Food_Item):
     def __init__(self):
@@ -28,16 +36,22 @@ class Pizza(Food_Item):
         
     def prepare(self):
         print( "Pizza boro ta deo dada  the burger patty and adding sauce ... ")
+    
+    def __repr__(self):
+        return self.get_name()
 
 class Drink(Food_Item):
     def __init__(self):
-        super().__init__("Lemon", 850)
+        super().__init__("Lemon_jus", 250)
         
     def prepare(self):
         print( "mojo boro ta deo dada  the burger patty and adding sauce ... ")
+        
+    def __repr__(self):
+        return self.get_name()
 
 class Order:
-    #self.items=[] ata dela sob aki jaga thka add hoba ..........
+    #self.items=[]                         ata dela sob aki       jaga thka add hoba   ..........
     def __init__(self):
         self.items=[]
     
@@ -54,22 +68,35 @@ class Order:
         total=0
         
         for item in self.items:
-            print(f"-{item}")
+            print(f"-{item} - {item.get_price()}")
             total+=item.get_price()
         print(f"Total Price :{total} BDT ")
+    
+    def __repr__(self):
+        items_name=" " 
+        for item in self.items:
+            items_name+=f"{item.get_name()}, "
+        return items_name
         
         
 Burger=Burger()
 Pizza=Pizza()
 drink=Drink()
-
+order=Order()
+order.add_item(Burger)
+order.add_item(Pizza)
  
-x_order=Order()
-#COUSTOMER
-x_order.add_item(Burger)
-x_order.add_item(Pizza)
+
+owner=Restrurant_Owener()
+owner.add_orders(order) 
+print(order.items)
+  
+# x_order=Order()
+# #COUSTOMER
+# x_order.add_item(Burger)
+# x_order.add_item(Pizza)
  
 # Proces
-x_order.process_order()
-x_order.show_order()
+# x_order.process_order()
+# x_order.show_order()
 
---
+++ week3/Con1.py/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+class Restrurant_Owener:
+    orderlist=[]
+    
+    def add_orders(self,order):
+        self.orderlist.append(order)
