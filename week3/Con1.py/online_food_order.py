c9fecf3a36a206a33f31518a66e2ff205bb458bc
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-04-23 07:57:22 +0600

    python

---
+++ week3/Con1.py/online_food_order.py
@@ -0,0 +1,75 @@
+
+from abc import ABC ,abstractmethod
+class Food_Item(ABC):
+    def __init__(self,name,price):
+        self.__name=name
+        self.__price=price
+        
+    def get_price(self):
+        return self.__price
+    
+    def get_name(self):
+        return self.__name
+    
+    @abstractmethod
+    def prepare(self):
+        pass
+    
+    
+class Burger(Food_Item):
+    def __init__(self):
+        super().__init__("Burger", 150)
+    def prepare(self):
+        print( "Grilling the burger patty and adding sauce ... ")
+
+class Pizza(Food_Item):
+    def __init__(self):
+        super().__init__("SPacye PIzza", 550)
+        
+    def prepare(self):
+        print( "Pizza boro ta deo dada  the burger patty and adding sauce ... ")
+
+class Drink(Food_Item):
+    def __init__(self):
+        super().__init__("Lemon", 850)
+        
+    def prepare(self):
+        print( "mojo boro ta deo dada  the burger patty and adding sauce ... ")
+
+class Order:
+    #self.items=[] ata dela sob aki jaga thka add hoba ..........
+    def __init__(self):
+        self.items=[]
+    
+    def add_item(self,food_item):
+        self.items.append(food_item)
+        
+    def process_order(self):
+        print("\n Prepraing your item")
+        for item in self.items:
+            item.prepare()
+    
+    def show_order(self):
+        print("\n Your Order Summery ")
+        total=0
+        
+        for item in self.items:
+            print(f"-{item}")
+            total+=item.get_price()
+        print(f"Total Price :{total} BDT ")
+        
+        
+Burger=Burger()
+Pizza=Pizza()
+drink=Drink()
+
+ 
+x_order=Order()
+#COUSTOMER
+x_order.add_item(Burger)
+x_order.add_item(Pizza)
+ 
+# Proces
+x_order.process_order()
+x_order.show_order()
+
