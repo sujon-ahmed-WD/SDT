7419613244a6db264890186794424bae66a9042c
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-04-26 22:25:21 +0600

    bus_truck

---
+++ final_exam/Bus_Ticket.py
@@ -0,0 +1,11 @@
+class Bus:
+    def __init__(self,number,route,total_seats,booked_seat):
+        self.number=number
+        self.route=route
+        self.total_seats=total_seats
+        self.booked_seat=0
+    
+    def available_seats(self,):
+      pass
+
+    def 
